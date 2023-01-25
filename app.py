1c180912a26661d0cb5a89638e37a93dde7bc01f
ri-gh <98031978+ri-gh@users.noreply.github.com>
2023-01-25 18:20:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+server = app.server
+
 os.system('python preprocess.py') #to make the preprocess of the data with the url provided
 
 os.system('python dashapp.py')  #to make the dashboard with plotly Dash
