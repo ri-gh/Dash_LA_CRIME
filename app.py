dd24d2ca359e722eaa4fa55ee1010d77261c1d59
ri-gh <98031978+ri-gh@users.noreply.github.com>
2023-01-25 17:39:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,5 @@ os.system('python preprocess.py') #to make the preprocess of the data with the u
 
 os.system('python dashapp.py')  #to make the dashboard with plotly Dash
 
+if __name__ == "__main__":
+    app.run_server(debug=False)
